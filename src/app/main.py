55d4ac477c388d87c8d56e60bb176574d810dad5
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-11-08 19:32:32 +0100

    REST: POST, placeholder

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,18 @@
 
 from fastapi import FastAPI
+from datetime import datetime, timezone
+from typing import Any
 
 app = FastAPI()
 
 @app.get("/status")
 def status():
     return {"status": "normal"}
+
+@app.post("/score")
+def score(data: dict[str, Any]):
+    return {
+        "received": data,
+        "timestamp": datetime.now(
+                                  timezone.utc).isoformat()
+    }
--- wfetch.bat
+++ wfetch.bat
@@ -1,1 +1,7 @@
+@echo FETCHING
 curl -s http://localhost:8000/status
+@echo 
+curl -s -X POST http://localhost:8000/score ^
+ -H "Content-Type: application/json" ^
+ -d "{\"temperature_c\": 28, \"humidity\": 61, \"sound\": 55}"
+
