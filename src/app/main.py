a301c1064a6df22dec38758c4ff5c5141fddb42e
rzhlm <118811618+rzhlm@users.noreply.github.com>
2025-11-09 20:44:52 +0100

    added actual scoring and threshold treatment + repaired fetch script (for testing)

--- src/app/main.py
+++ src/app/main.py
@@ -20,34 +20,65 @@ def calc_score(temp_c: float, humidity_pct: float, sound_db: float) -> float:
     # both negative and positive differences are bad
     # 
     if temp_c == -99: # sensor out of order
-        temp_score == -1 # flag sensor being OoO
+        temp_score = -1 # flag sensor being OoO
     elif (18 <= temp_c <= 24):
         temp_score = 0
     else:
-        # range -270°C to 1500°C
-        temp_score = abs(temp_c - 21)
+        # possible temp range -270°C to 1500°C
+        # realistically, no production if:
+        # temp < 5°C or temp > 60°C
+        # so total useful range is 55°C
+        if temp_c <= 5:
+            temp_c = 5
+        elif temp_c > 60:
+            temp_c = 60
+        temp_score = abs(temp_c - 21) # between 21-5 and 55-21
+        # := 16 and 34
+        # normalize:
+        temp_score /= 34 # 55
+        # between 0 and 0.618
+    
+    print(f"{temp_c=}; {temp_score=}")
     
     # Humidity %: humidity_pct
     #
     # any humidity different from 60% is not good.
     if humidity_pct == -99:
-        temp_score = -1
+        humid_score = -1 # out of order
     else:
         # range 0% to 100%
-        humid_score = abs(humidity_pct - 0.60)
+        humid_score = abs(humidity_pct - 60)
+        # thus, between 0 and 40
+        # back to a float%
+        humid_score /= 40
+    print(f"{humidity_pct=}; {humid_score=}")
+
 
     # Sound dB: sound_db
     #
     # any sound different than 50 dB is not good.
     if sound_db == -99:
-        sound_score = -1
+        sound_score = -1 # out of order
     else:
-        # range 0 to 310 dB
-        sound_score = abs(sound_db - 50)
-        
-
+        # possible range 0 to 310 dB
+        # realistically:
+        # more than 50 db is bad, if it's higher than 90 something is wrong,
+        # less means something is wrong
+        # working range: 0 to 90
+        if sound_db >= 90:
+            sound_db = 90
+        sound_score = abs(sound_db - 50) # between 0 and 50
+        sound_score /= 50 # between 0 and 0.56
+    print(f"{sound_db=}; {sound_score=}")
     
-    score = 0.95
+
+    # TODO: treat out-of-order values and ignore that sensor
+    score = temp_score + humid_score + sound_score
+    if score > 1:
+        score = 1
+    print(f"{score=}")
+    
+   # score = 0.95
     
     return score
 
--- wfetch.bat
+++ wfetch.bat
@@ -3,5 +3,5 @@ curl -s http://localhost:8000/status
 @echo 
 curl -s -X POST http://localhost:8000/score ^
  -H "Content-Type: application/json" ^
- -d "{\"temperature_c\": 28, \"humidity\": 61, \"sound\": 55}"
+ -d "{\"temperature_c\": 28, \"humidity_pct\": 61, \"sound_db\": 55}"
 
